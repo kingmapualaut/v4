46f47ea6b31bd17e2e36446af1d0a06393058baa
kingmapualaut <70792921+kingmapualaut@users.noreply.github.com>
2020-11-12 13:33:01 +0800

    Create vrom.sh

---
+++ vrom.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+# Created by https://www.hostingtermurah.net
+# Modified by 0123456
+
+#Requirement
+if [ ! -e /usr/bin/curl ]; then
+    apt-get -y update && apt-get -y upgrade
+	apt-get -y install curl
+fi
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(curl -4 icanhazip.com)
+if [ $MYIP = "" ]; then
+   MYIP=`ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1`;
+fi
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# go to root
+cd
+
+# install squid3
+apt-get -y install squid
+cat > /etc/squid/squid.conf <<-END
+acl VPN dst IP-ADDRESS/32
+acl all src 0.0.0.0/0
+http_access allow all
+http_access allow VPN
+http_access deny all 
+http_port 8080
+http_port 8000
+http_port 3128
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+acl SSH dst 128.199.157.230-128.199.157.230/255.255.255.255
+acl SSH dst 178.128.18.93-178.128.18.93/255.255.255.255
+acl SSH dst 178.128.61.6-178.128.61.6/255.255.255.255
+acl SSH dst 159.65.13.145-159.65.13.145/255.255.255.255
+acl SSH dst 128.199.111.9-128.199.111.9/255.255.255.255
+acl SSH dst 159.65.140.10-159.65.140.10/255.255.255.255
+acl SSH dst 178.128.219.61-178.128.219.61/255.255.255.255
+acl SSH dst 128.199.198.111-128.199.198.111/255.255.255.255
+acl SSH dst 115.164.5.48-115.164.5.48/255.255.255.255
+acl SSH dst 115.164.14.11-115.164.14.11/255.255.255.255
+acl SSH dst 121.123.228.74-121.123.228.74/255.255.255.255
+acl SSH dst 13.250.162.203-13.250.162.203/255.255.255.255
+### Allow Headers
+request_header_access Allow allow all 
+request_header_access Authorization allow all 
+request_header_access WWW-Authenticate allow all 
+request_header_access Proxy-Authorization allow all 
+request_header_access Proxy-Authenticate allow all 
+request_header_access Cache-Control allow all 
+request_header_access Content-Encoding allow all 
+request_header_access Content-Length allow all 
+request_header_access Content-Type allow all 
+request_header_access Date allow all 
+request_header_access Expires allow all 
+request_header_access Host allow all 
+request_header_access If-Modified-Since allow all 
+request_header_access Last-Modified allow all 
+request_header_access Location allow all 
+request_header_access Pragma allow all 
+request_header_access Accept allow all 
+request_header_access Accept-Charset allow all 
+request_header_access Accept-Encoding allow all 
+request_header_access Accept-Language allow all 
+request_header_access Content-Language allow all 
+request_header_access Mime-Version allow all 
+request_header_access Retry-After allow all 
+request_header_access Title allow all 
+request_header_access Connection allow all 
+request_header_access Proxy-Connection allow all 
+request_header_access User-Agent allow all 
+request_header_access Cookie allow all 
+request_header_access All deny all
+### HTTP Anonymizer Paranoid
+reply_header_access Allow allow all 
+reply_header_access Authorization allow all 
+reply_header_access WWW-Authenticate allow all 
+reply_header_access Proxy-Authorization allow all 
+reply_header_access Proxy-Authenticate allow all 
+reply_header_access Cache-Control allow all 
+reply_header_access Content-Encoding allow all 
+reply_header_access Content-Length allow all 
+reply_header_access Content-Type allow all 
+reply_header_access Date allow all 
+reply_header_access Expires allow all 
+reply_header_access Host allow all 
+reply_header_access If-Modified-Since allow all 
+reply_header_access Last-Modified allow all 
+reply_header_access Location allow all 
+reply_header_access Pragma allow all 
+reply_header_access Accept allow all 
+reply_header_access Accept-Charset allow all 
+reply_header_access Accept-Encoding allow all 
+reply_header_access Accept-Language allow all 
+reply_header_access Content-Language allow all 
+reply_header_access Mime-Version allow all 
+reply_header_access Retry-After allow all 
+reply_header_access Title allow all 
+reply_header_access Connection allow all 
+reply_header_access Proxy-Connection allow all 
+reply_header_access User-Agent allow all 
+reply_header_access Cookie allow all 
+reply_header_access All deny all
+### CoreDump
+coredump_dir /var/spool/squid
+dns_nameservers 8.8.8.8 8.8.4.4
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname JohnFordTV
+END
+sed -i $MYIP2 /etc/squid/squid.conf;
+service squid restart
+
+wget https://raw.githubusercontent.com/TongGakod/sultan/main/googlecloud && bash googlecloud && rm googlecloud
