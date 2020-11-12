8adfa165f56b089697005ef76335b5ac77271dc8
kingmapualaut <70792921+kingmapualaut@users.noreply.github.com>
2020-11-12 13:37:21 +0800

    Update vrom.sh

--- vrom.sh
+++ vrom.sh
@@ -30,20 +30,6 @@ http_access deny all
 http_port 8080
 http_port 8000
 http_port 3128
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
-acl SSH dst 128.199.157.230-128.199.157.230/255.255.255.255
-acl SSH dst 178.128.18.93-178.128.18.93/255.255.255.255
-acl SSH dst 178.128.61.6-178.128.61.6/255.255.255.255
-acl SSH dst 159.65.13.145-159.65.13.145/255.255.255.255
-acl SSH dst 128.199.111.9-128.199.111.9/255.255.255.255
-acl SSH dst 159.65.140.10-159.65.140.10/255.255.255.255
-acl SSH dst 178.128.219.61-178.128.219.61/255.255.255.255
-acl SSH dst 128.199.198.111-128.199.198.111/255.255.255.255
-acl SSH dst 115.164.5.48-115.164.5.48/255.255.255.255
-acl SSH dst 115.164.14.11-115.164.14.11/255.255.255.255
-acl SSH dst 121.123.228.74-121.123.228.74/255.255.255.255
-acl SSH dst 13.250.162.203-13.250.162.203/255.255.255.255
 ### Allow Headers
 request_header_access Allow allow all 
 request_header_access Authorization allow all 
